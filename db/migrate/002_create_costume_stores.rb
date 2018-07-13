a49cf0d24c9459c9dd2d17309c58476d01491644
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-13 12:38:53 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,8 +2,17 @@
 
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
-
-
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory 
+      t.integer :num_of_employees
+      t.boolean :still_in_business 
+      t.integer :start_time 
+      t.integer :end_time
+    end
+  end
 
 
 end
