68790ce13030f3da1293eea1f7850c0d5c22ad19
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-13 12:53:19 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.integer :price
       t.boolean :family_friendly
       t.date :opening_date
-      t.datetime :closing_date
+      t.date :closing_date
       t.string :description
     end
   end
